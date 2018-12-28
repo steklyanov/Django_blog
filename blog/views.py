5d1e84ecbb1ebc6d6a4331a3f4ac0509c0527cd3
Maron martell Mraz <steklyanov@gmail.com>
2018-12-28 17:27:09 +0300

    lesson 4, minute 25

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 
 def posts_list(request):
-    n = ['Max', 'Dasha', 'Kolya', 'Ksu']
-    return render(request, 'blog/index.html', context={'names': n})
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
+
+def post_detail(request, slug):
+
