ea3af4f39d09f8ad181ae576f84137890a66b465
Maron martell Mraz <steklyanov@gmail.com>
2018-12-30 11:34:17 +0300

    delete repeat

--- blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('', posts_list, name='posts_list_url'),
-    path('post/<str:slug>/', post_detail, mane='post_detail_url')
-]
