3a02267e5ce5ac88dc63b6942f39aa8a6fef2557
Maron martell Mraz <steklyanov@gmail.com>
2019-01-04 23:58:28 +0300

    finish lesson 9, done for today

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', posts_list, name='posts_list_url'),
     path('post/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),  # as_view точка входа при вызове класса
+    path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_updates_url'),
     path('tags/', tags_list, name='tags_list_url'),  # в нем создается экземпляр класса и вызывается
     path('tag/create/', TagCreate.as_view(), name='tag_create_url'),  # соответствующий запросу метод
     path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
--- blog/views.py
+++ blog/views.py
@@ -23,6 +23,12 @@ class PostCreate(ObjectCreateMixin, View):
     template = 'blog/post_create_form.html'
 
 
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
+
+
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
@@ -45,20 +51,10 @@ class TagCreate(ObjectCreateMixin, View):
     #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
-class TagUpdate(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(instance=tag)
-        return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
-
-    def post(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        bound_form = TagForm(request.POST, instance=tag)
-
-        if bound_form.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/tag_update_form', context={'form': bound_form, 'tag': tag})
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
 
 
 def tags_list(request):
